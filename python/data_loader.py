d92090699667a9042a9f16d46ff10173192f071f
JonoVincent-PorAI <yz24943@bristol.ac.uk>
2025-06-23 11:43:35 +0100

    Minor Fix to probe_batch()

--- python/data_loader.py
+++ python/data_loader.py
@@ -288,7 +288,7 @@ class Data_Loader():
 
         
         for output in extra_outputs:
-            directory_name = save_directory + model_name + '/' + (output.split('.')[0])
+            directory_name = save_directory + '/' + model_name + '/' + (output.split('.')[0])
             directory_path = Path(directory_name)
             try:
                 directory_path.mkdir(parents=True, exist_ok=True)
@@ -297,7 +297,7 @@ class Data_Loader():
                 print(print(f"Directory '{directory_name}' already exists. Writing outputs to '{directory_name}"))
 
         
-        meta_file = Path(save_directory + model_name + '/meta_data.npy')
+        meta_file = Path(save_directory + '/' + model_name + '/meta_data.npy')
 
 
         if not meta_file.is_file():
@@ -356,7 +356,7 @@ class Data_Loader():
                     
 
             for output in extra_outputs:
-                directory_name = save_directory + model_name + '/' + (output.split('.')[0])
+                directory_name = save_directory + '/' + model_name + '/' + (output.split('.')[0])
                 file_name = 'game_' + str(index) + '.npy'
                 out_file = directory_name + '/' + file_name
                 output_array = np.array(probe_outputs[output])
