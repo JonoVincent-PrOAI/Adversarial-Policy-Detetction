4171a7bf2ef85582e7ddbff5f7fa63a1455180e1
JonoVincent-PrOAI <yz24943@bristol.ac.uk>
2025-07-10 15:33:53 +0100

    add utilities needed for eval

--- python/adv_det_dataset.py
+++ python/adv_det_dataset.py
@@ -15,6 +15,7 @@ class Adversarial_Detection_Dataset(Dataset):
                 game_data = np.load(game_file, allow_pickle=True)
                 self.game_length_index.append(len(game_data))
 
+
     def __getitem__(self, idx):
         game_count = 0
         game_index = -1    
@@ -35,5 +36,44 @@ class Adversarial_Detection_Dataset(Dataset):
                 sample['game num'] = game_index
                 return(sample['layer activation'], sample['adversarial'])
 
+
+    def get_sample(self, idx):
+
+        game_count = 0
+        game_index = -1 
+
+        while idx >= game_count:
+            game_index = game_index + 1
+            game_count = game_count + self.game_length_index[game_index]
+        
+        if game_index > len(self.game_length_index):
+            print('Error: Index' + str(idx) + ' out of range for dataset of size ' + 
+                  str(sum(self.game_length_index)))
+        else:
+            move_index = idx - sum(self.game_length_index[: game_index])
+            game_file = os.listdir(self.data_dir)[game_index]
+            game_file_path = self.dir_path + '/' + os.fsdecode(game_file)
+            with open(game_file_path, 'rb') as f:
+                game_data = np.load(f, allow_pickle=True)
+                sample = game_data[move_index]
+                sample['game num'] = game_index
+                return(sample)
+            
+    def get_game_num_from_idx(self, idx):
+
+        game_count = 0
+        game_index = -1
+
+        while idx >= game_count:
+            game_index = game_index + 1
+            game_count = game_count + self.game_length_index[game_index]
+        
+        if game_index > len(self.game_length_index):
+            print('Error: Index' + str(idx) + ' out of range for dataset of size ' + 
+                  str(sum(self.game_length_index)))
+            
+        else:
+            return(game_index)
+            
     def __len__(self):
         return(sum(self.game_length_index))
